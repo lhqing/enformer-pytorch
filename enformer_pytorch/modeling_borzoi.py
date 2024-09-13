a1ac6acb5afef632f28cc3d5e0bab2c1a147c730
Hanqing Liu <hanqingliu@fas.harvard.edu>
2024-09-13 08:39:41 -0700

    cleanup borzoi code

--- enformer_pytorch/modeling_borzoi.py
+++ enformer_pytorch/modeling_borzoi.py
@@ -7,7 +7,11 @@ import math
 from torch import nn, einsum
 
 from einops import rearrange
-from .modeling_enformer import relative_shift, Residual, TargetLengthCrop
+from .modeling_enformer import (
+    relative_shift,
+    Residual,
+    TargetLengthCrop,
+)
 from bolero.tl.generic.module_lora_cond import ConditionalLoRALayer
 
 
@@ -145,10 +149,13 @@ class Attention(nn.Module):
 
 
 class ConvDna(nn.Module):
-    def __init__(self):
+    def __init__(self, in_channels=4, out_channels=512, dna_kernel_size=15):
         super(ConvDna, self).__init__()
         self.conv_layer = nn.Conv1d(
-            in_channels=4, out_channels=512, kernel_size=15, padding="same"
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=dna_kernel_size,
+            padding="same",
         )
         self.max_pool = nn.MaxPool1d(kernel_size=2, padding=0)
 
@@ -210,12 +217,73 @@ class FeedForward(nn.Sequential):
         return x
 
 
-class Borzoi(nn.Module):
+class TransformerLayer(nn.Module):
+    def __init__(
+        self,
+        channels=1536,
+        heads=8,
+        dim_key=64,
+        dim_value=192,
+        dropout=0.05,
+        pos_dropout=0.01,
+        attention_dropout=0.2,
+        feedforward_dropout=0.2,
+        num_rel_pos_features=32,
+    ):
+        self.layers = SequentialwithArgs(
+            Residual(
+                SequentialwithArgs(
+                    nn.LayerNorm(channels, eps=0.001),
+                    Attention(
+                        channels,
+                        heads=heads,
+                        dim_key=dim_key,
+                        dim_value=dim_value,
+                        dropout=dropout,
+                        pos_dropout=pos_dropout,
+                        num_rel_pos_features=num_rel_pos_features,
+                    ),
+                    nn.Dropout(attention_dropout),
+                )
+            ),
+            Residual(
+                FeedForward(
+                    input_dim=channels,
+                    hidden_dim=channels * 2,
+                    output_dim=channels,
+                    dropout=feedforward_dropout,
+                ),
+            ),
+        )
 
-    def __init__(self, checkpoint_path=None):
-        super(Borzoi, self).__init__()
-        self.conv_dna = ConvDna()
+    def forward(self, x, *args, **kwargs):
+        return self.layers(x, *args, **kwargs)
+
+
+class Borzoi(nn.Module):
+    default_config = {
+        "checkpoint_path": None,
+    }
+
+    def __init__(
+        self,
+        checkpoint_path,
+    ):
+        super().__init__()
+
+        # =========
+        # Conv DNA
+        # =========
+        self.conv_dna = ConvDna(
+            in_channels=4,
+            out_channels=512,
+            dna_kernel_size=15,
+        )
         self._max_pool = nn.MaxPool1d(kernel_size=2, padding=0)
+
+        # ==============
+        # Residual Tower
+        # ==============
         self.res_tower = SequentialwithArgs(
             ConvBlock(in_channels=512, out_channels=608, kernel_size=5),
             self._max_pool,
@@ -227,39 +295,37 @@ class Borzoi(nn.Module):
             self._max_pool,
             ConvBlock(in_channels=1056, out_channels=1280, kernel_size=5),
         )
+
+        # ==================
+        # UNet connections 1
+        # ==================
         self.unet1 = SequentialwithArgs(
             self._max_pool,
             ConvBlock(in_channels=1280, out_channels=1536, kernel_size=5),
         )
-        transformer = []
-        for _ in range(8):
-            transformer.append(
-                SequentialwithArgs(
-                    Residual(
-                        SequentialwithArgs(
-                            nn.LayerNorm(1536, eps=0.001),
-                            Attention(
-                                1536,
-                                heads=8,
-                                dim_key=64,
-                                dim_value=192,
-                                dropout=0.05,
-                                pos_dropout=0.01,
-                                num_rel_pos_features=32,
-                            ),
-                            nn.Dropout(0.2),
-                        )
-                    ),
-                    Residual(
-                        FeedForward(
-                            input_dim=1536,
-                            hidden_dim=1536 * 2,
-                            output_dim=1536,
-                            dropout=0.2,
-                        ),
-                    ),
-                )
+
+        # ===========
+        # Transformer
+        # ===========
+        transformer = [
+            TransformerLayer(
+                channels=1536,
+                heads=8,
+                dim_key=64,
+                dim_value=192,
+                dropout=0.05,
+                pos_dropout=0.01,
+                attention_dropout=0.2,
+                feedforward_dropout=0.2,
+                num_rel_pos_features=32,
             )
+            for _ in range(8)
+        ]
+        self.transformer = SequentialwithArgs(*transformer)
+
+        # ===========================
+        # UNet horizontal connections
+        # ===========================
         self.horizontal_conv0 = ConvBlock(
             in_channels=1280, out_channels=1536, kernel_size=1
         )
@@ -267,8 +333,10 @@ class Borzoi(nn.Module):
             in_channels=1536, out_channels=1536, kernel_size=1
         )
 
+        # ===================================
+        # UNet upsampling and separable convs
+        # ===================================
         self.upsample = torch.nn.Upsample(scale_factor=2)
-        self.transformer = SequentialwithArgs(*transformer)
 
         self.upsampling_unet1 = SequentialwithArgs(
             ConvBlock(in_channels=1536, out_channels=1536, kernel_size=1),
@@ -286,6 +354,9 @@ class Borzoi(nn.Module):
             in_channels=1536, out_channels=1536, kernel_size=3, conv_type="separable"
         )
 
+        # ===================
+        # Final Crop and Conv
+        # ===================
         self.crop = TargetLengthCrop(16384 - 32)
         self.final_joined_convs = SequentialwithArgs(
             ConvBlock(in_channels=1536, out_channels=1920, kernel_size=1),
@@ -297,42 +368,69 @@ class Borzoi(nn.Module):
             self.load_state_dict(torch.load(checkpoint_path))
 
     def forward(self, x, *args, **kwargs):
-        # x: (bs, 4, 524288)
+        # =================
+        # DNA Convolution
+        # In - x: (bs, 4, 524288)
+        # Out - x: (bs, 512, 262144)
+        # signal resolution is 1
+        # =================
         x = self.conv_dna(x, *args, **kwargs)
-        # x: (bs, 512, 262144)
+
+        # =================
+        # Residual Tower (7 blocks)
+        # In - x: (bs, 512, 262144)
+        # Out - x_unet0: (bs, 1536, 16384)
+        #       x_unet1: (bs, 1536, 8192)
+        #       x: (bs, 1536, 4096)
+        # signal resolution is 128
+        # =================
         x_unet0 = self.res_tower(x, *args, **kwargs)
-        # x_unet0: (bs, 1280, 16384)
         x_unet1 = self.unet1(x_unet0, *args, **kwargs)
-        # x_unet1: (bs, 1536, 8192)
         x = self._max_pool(x_unet1)
-        # x: (bs, 1536, 4096)
-
-        x_unet1 = self.horizontal_conv1(x_unet1, *args, **kwargs)
-        # x_unet1: (bs, 1536, 8192)
+        # horizontal convolution before unet connections
+        # conv 1x1, 1280 -> 1536 channels
         x_unet0 = self.horizontal_conv0(x_unet0, *args, **kwargs)
-        # x_unet0: (bs, 1536, 16384)
+        # conv 1x1, 1536 -> 1536 channels
+        x_unet1 = self.horizontal_conv1(x_unet1, *args, **kwargs)
 
+        # =================
+        # Transformer
+        # In - x: (bs, 1536, 4096)
+        # Out - x: (bs, 1536, 4096)
+        # signal resolution is 128
+        # =================
         x = self.transformer(x.permute(0, 2, 1), *args, **kwargs)
         x = x.permute(0, 2, 1)
-        # x: (bs, 1536, 4096)
 
+        # =================
+        # UNet upsampling and separable convs 1
+        # In - x: (bs, 1536, 4096)
+        # Out - x: (bs, 1536, 8192)
+        # signal resolution is 64
+        # =================
         x = self.upsampling_unet1(x, *args, **kwargs)
-        # x: (bs, 1536, 8192)
         x += x_unet1
         x = self.separable1(x, *args, **kwargs)
-        # x: (bs, 1536, 8192)
 
+        # =================
+        # UNet upsampling and separable convs 0
+        # In - x: (bs, 1536, 8192)
+        # Out - x: (bs, 1536, 16384)
+        # signal resolution is 32
+        # =================
         x = self.upsampling_unet0(x, *args, **kwargs)
-        # x: (bs, 1536, 16384)
         x += x_unet0
-        # x: (bs, 1536, 16384)
         x = self.separable0(x, *args, **kwargs)
-        # x: (bs, 1536, 16384)
 
-        x = self.crop(x.permute(0, 2, 1))
-        # x: (bs, 16352, 1536)
-        x = self.final_joined_convs(x.permute(0, 2, 1), *args, **kwargs)
-        # x: (bs, 1920, 16352)
+        # =================
+        # Final Crop and Conv
+        # In - x: (bs, 1536, 16384)
+        # Out - x: (bs, 1920, 16352)
+        # signal resolution is 32
+        # =================
+        x = self.crop(x.permute(0, 2, 1)).permute(0, 2, 1)
+        x = self.final_joined_convs(x, *args, **kwargs)
+        return x
 
 
 class BorzoiWithOutputHead(Borzoi):
@@ -356,9 +454,12 @@ class BorzoiWithOutputHead(Borzoi):
 
         human_out = self.final_softplus(self.human_head(x))
         # human_out: (bs, 7611, 16352)
+        # equivalent to 16352*32 = 523264 bp signal
+
         if self.enable_mouse_head:
             mouse_out = self.final_softplus(self.mouse_head(x))
             # mouse_out: (bs, 2608, 16352)
+            # equivalent to 16352*32 = 523264 bp signal
             return human_out, mouse_out
         else:
             return human_out
---
+++ test/test_basic.py
@@ -0,0 +1,5 @@
+# %%
+from enformer_pytorch.modeling_enformer import exponential_linspace_int
+# %%
+exponential_linspace_int(512,1536,7,32)
+# %%
